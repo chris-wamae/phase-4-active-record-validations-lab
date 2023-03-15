a4e3a4521f0870813269150ba418236cb37804a8
chris-wamae <chriswamae123@gmail.com>
2023-03-15 12:19:45 +0300

    Completed Assignent

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title ,presence: true
+    validates :content, length:{minimum:250}
+    validates :summary, length:{maximum:250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :validate_title
+
+    def validate_title
+        if title == "True Facts"
+            errors.add(:title, "Invalid title")
+        else
+        end
+    end 
+
+
 end
